477827b07f0cc1159c1b8c9f3ad802235de2f8d0
vava0807 <128585771+vava0807@users.noreply.github.com>
2026-01-25 01:54:07 +0800

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
 // Firebase 配置
 const firebaseConfig = {
   // 在這裡填入你的 Firebase 專案配置
-  // 請到 Firebase Console 獲取這些資訊：https://console.firebase.google.com/
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  // 請到 Firebase Console 取得這些資訊：https://console.firebase.google.com/
+  apiKey: "AIzaSyDpuLk-g7tSbhulifMtW1qFpR9HhBX3fM8",
+  authDomain: "my-2026-system.firebaseapp.com",
+  databaseURL: "https://my-2026-system-default-rtdb.firebaseio.com",
+  projectId: "my-2026-system",
+  storageBucket: "my-2026-system.firebasestorage.app",
+  messagingSenderId: "117966706809",
+  appId: "1:117966706809:web:073930b6ba501e0ae7dca8",
+  measurementId: "G-Q81S79YY54"
 };
 
 // 初始化 Firebase
